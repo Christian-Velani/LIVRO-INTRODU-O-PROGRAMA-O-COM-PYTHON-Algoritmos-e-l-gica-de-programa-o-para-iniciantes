24f6376c5abd5e0c63bb8a8ebe728c33963b4b79
Christian Velani <chris13082000@gmail.com>
2022-06-06 18:00:30 +0000

    Fiz o Exercicio 9.22

--- Programa_9_6.py
+++ Programa_9_6.py
@@ -9,8 +9,13 @@
 #Exercício 9.21 Nas funções de alterar e apaga , peça que o usuário confirme a alteração
 #e exclusão do nome antes de realizar a operação em si.
 
+#Exercício 9.22 Ao ler ou gravar uma nova lista, verifique se a agenda atual já foi gravada.
+#Você pode usar uma variável para controlar quando a lista foi alterada (novo,
+#altera, apaga) e reinicializar esse valor quando ela for lida ou gravada.
+
 # Programa 9.6 - Controle de uma agenda de telefones
 agenda = []
+status = 'Salvo'
 def pede_nome():
     return input('Nome: ')
 def pede_telefone():
@@ -27,11 +32,14 @@ def pesquisa(nome):
     return None
 def novo():
     global agenda
+    global status
     nome = pede_nome()
     telefone = pede_telefone()
     agenda.append([nome, telefone])
+    status = 'Não salvo'
 def apaga():
     global agenda
+    global status
     nome = pede_nome()
     p = pesquisa(nome)
     confirmar = input('Tem certeza que quer excluir o contato?(S/N)').upper()
@@ -39,7 +47,9 @@ def apaga():
         del agenda[p]
     else:
         print('Nome não encontrado.')
+    status = 'Não salvo'
 def altera():
+    global status
     p = pesquisa(pede_nome())
     if p is not None:
         nome = agenda[p][0]
@@ -53,6 +63,7 @@ def altera():
             agenda[p] = [nome, telefone]
     else:
         print('Nome não encontrado.')
+    status = 'Não salvo'
 def lista():
     print('\nAgenda\n\n------')
     for i, e in enumerate(agenda):
@@ -61,17 +72,21 @@ def lista():
     print('------\n')
 def lê():
     global agenda
+    global status
     nome_arquivo = pede_nome_arquivo()
     with open(nome_arquivo, 'r') as arquivo:
         agenda = []
         for l in arquivo.readlines():
             nome, telefone = l.strip().split('#')
             agenda.append([nome, telefone])
+    status = 'Salvo'
 def grava():
+    global status
     nome_arquivo = pede_nome_arquivo()
     with open(nome_arquivo, 'w') as arquivo:
         for e in agenda:
             arquivo.write(f'{e[0]}#{e[1]}\n')
+    status = 'Salvo'
 def valida_faixa_inteiro(pergunta, inicio, fim):
     while True:
         try:
@@ -85,6 +100,7 @@ def ordenar():
     agenda = sorted(agenda, key= lambda agenda: agenda[0])
 def menu():
     print(f"""
+    Status da lista atual: {status}
     Tamanho atual da agenda: {len(agenda)}
 
     1 - Novo
