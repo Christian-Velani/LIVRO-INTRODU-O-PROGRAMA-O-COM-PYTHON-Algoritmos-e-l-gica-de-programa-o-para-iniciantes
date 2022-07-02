c15063ccfd6447a18a577cf1ee6d936c81414a3a
Christian Velani <chris13082000@gmail.com>
2022-07-02 11:51:49 +0000

    Criei o Programa 9.9

---
+++ programa_9_9.py
@@ -0,0 +1,6 @@
+# Programa 9.9 - Verificação se um diretório ou arquivo já existe
+import os.path
+if os.path.exists('z'):
+    print('O diretório z existe.')
+else:
+    print('O diretório z não existe.')
