5b160fe4afc10e5a74fd6745f153cbeffd917a94
Christian Velani <chris13082000@gmail.com>
2022-07-04 15:34:38 +0000

    Modifiquei o teste.py

--- teste.py
+++ teste.py
@@ -1,13 +1,14 @@
 from clientes import Cliente
 from contas import Conta
-joao = Cliente( "João da Silva ", "777-1234 ")
+Joao = Cliente( "João da Silva ", "777-1234 ")
 Maria = Cliente( "Maria da Silva ", "555-4321")
 
-conta = Conta((joao, Maria), '11113101812101010', 1500)
-conta.resumo()
-conta.saque(1000)
-conta.resumo()
-conta.saque(50)
-conta.resumo()
-conta.deposito(200)
-conta.resumo()
+conta1 = Conta((Joao), 1, 1000)
+conta2 = Conta([Maria, Joao], 2, 500)
+conta1.saque(50)
+conta2.deposito(300)
+conta1.saque(190)
+conta2.deposito(95.15)
+conta2.saque(250)
+conta1.extrato()
+conta2.extrato()
