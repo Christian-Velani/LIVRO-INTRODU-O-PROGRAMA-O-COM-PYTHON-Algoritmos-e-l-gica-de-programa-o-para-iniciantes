00bcef2ef29687d6dc6b19a16d8b6c9f0b4ca945
Christian Velani <chris13082000@gmail.com>
2022-07-08 10:41:39 -0300

    Adicionei a classe ContaEspecial

--- contas.py
+++ contas.py
@@ -5,6 +5,9 @@
 
 #Exercicio 10.7 - Modifique o método resumo da classe Conta para exibir
 #o nome eo telefone de cada cliente
+from pydoc import cli
+
+
 class Conta:
     def __init__(self, clientes, numero, saldo = 0):
         self.saldo = 0
@@ -30,3 +33,12 @@ class Conta:
         for o in self.operacoes:
             print(f'{o[0]:10s} {o[1]:10.2f}')
         print(f'\n  Saldo: {self.saldo:10.2f}\n')
+
+class ContaEspecial(Conta):
+    def __init__(self, clientes, número, saldo=0, limite=0):
+        Conta.__init__(self, clientes, número, saldo)
+        self.limite = limite
+    def saque(self, valor):
+        if self.saldo + self.limite >= valor:
+            self.saldo -= valor
+            self.operacoes.append(['SAQUE', valor])
