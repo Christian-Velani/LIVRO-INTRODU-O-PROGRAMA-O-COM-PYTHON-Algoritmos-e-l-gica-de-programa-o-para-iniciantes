ed0d24dc2bffcd6512876bb9465be95bcb7806f1
Christian Velani <chris13082000@gmail.com>
2022-07-08 09:15:34 -0300

    Realizei o Exercício 10.7

--- contas.py
+++ contas.py
@@ -1,6 +1,10 @@
 # Programa 10.1 - Conta com registro de operações e extrato
+
 #Exercício 10.6 - Altere o programa de forma que a mensagem saldo insuficiente
 #seja exibida caso haja tentativa de sacar mais dinheiro que o saldo disponível
+
+#Exercicio 10.7 - Modifique o método resumo da classe Conta para exibir
+#o nome eo telefone de cada cliente
 class Conta:
     def __init__(self, clientes, numero, saldo = 0):
         self.saldo = 0
@@ -9,6 +13,8 @@ class Conta:
         self.operacoes = []
         self.deposito(saldo)
     def resumo(self):
+        for i in self.clientes:
+            print(f'Nome: {i.nome} Telefone: {i.telefone}') #Este código não funciona para contas com apenas 1 cliente
         print(f"CC Número: {self.numero} Saldo: {self.saldo: 10.2f}")
     def saque(self, valor):
         if self.saldo >= valor:
